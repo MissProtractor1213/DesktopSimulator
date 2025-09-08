dafcfeed7c73c06c5de140cb088c46ca04040998
MissProtractor1213 <tranwedding1213@gmail.com>
2025-09-08 14:16:38 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";                         // keep your global styles
+import EscapeRoomDesktop from "./EscapeRoomDesktop"; // <-- your big component file
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <EscapeRoomDesktop />
   </React.StrictMode>
 );
